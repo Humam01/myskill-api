d157ea88d5b8728ac81c1d8f2f8e3a7b916599af
Humam Ardiansyah <humamardiansyah672@gmail.com>
2025-05-20 17:10:55 +0700

    Sanctum auth sanctum API

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,16 @@ use App\Http\Controllers\ProductController;
 
 
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/save', [ProductController::class, 'save'])->name('product.save');
+    Route::delete('/{id}/delete', [ProductController::class, 'delete'])->name('product.delete');
+});
 
-Route::post('/save', [ProductController::class, 'save'])->name('product.save');
+Route::get('/user', function(Request $request){
+    return $request->user();
+});
+
 Route::post('/login',[AuthController::class,'login'])->name('product.logn');
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
-Route::delete('/{id}/delete', [ProductController::class, 'delete'])->name('product.delete');
+
